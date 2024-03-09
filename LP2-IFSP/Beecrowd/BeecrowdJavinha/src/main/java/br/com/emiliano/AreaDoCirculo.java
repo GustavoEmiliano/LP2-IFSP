4ad31736e1238d00d086d499938e40b842b69bba
Gustavo Emiliano <127131203+GustavoEmiliano@users.noreply.github.com>
2024-03-09 18:38:27 -0300

    beecrowd

---
+++ LP2-IFSP/Beecrowd/BeecrowdJavinha/src/main/java/br/com/emiliano/AreaDoCirculo.java
@@ -0,0 +1,10 @@
+package br.com.emiliano;
+
+import java.io.IOException;
+
+public class AreaDoCirculo {
+    public static void main(String[] args) throws IOException {
+
+
+    }
+}
---
+++ LP2-IFSP/Beecrowd/BeecrowdJavinha/src/main/java/br/com/emiliano/HelloWorld.java
@@ -0,0 +1,12 @@
+package br.com.emiliano;
+
+import java.io.IOException;
+
+public class HelloWorld {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+
+    }
+}
+
